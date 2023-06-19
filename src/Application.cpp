b9255edcc170a96714807d6989dd5a7d8eb3a620
CooperCX <15801789097@163.com>
2023-06-19 12:29:25 +0800

    feat: first commit

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,7 +3,7 @@
 int main(void)
 {
     GLFWwindow* window;
-
+    
     /* Initialize the library */
     if (!glfwInit())
         return -1;
@@ -26,9 +26,11 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
+
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
+
         glEnd();
 
         /* Swap front and back buffers */
